b606ad90fd7624c20e9fc6430e2fa85282a8dfc7
Aldmi1988 <aldmi@bk.ru>
2020-05-13 12:21:11 +0700

    DependentInseartServiceTest completed

--- src/Shared.Test/StringInseartService/DependentInseartTest/DependentInseartServiceTest.cs
+++ src/Shared.Test/StringInseartService/DependentInseartTest/DependentInseartServiceTest.cs
@@ -21,7 +21,7 @@ namespace Shared.Test.StringInseartService.DependentInseartTest
                  "{NumberOfCharacters:X2}\\\"{NumberOfTrain}\\\"{NumberOfCharacters:X2}\\\"{TArrival:t}\\\"{NumberOfCharacters:X2}\\\"{PathNumber}\\\"",
                  "",
                  "Windows-1251",
-                 new StringBuilder("{NumberOfCharacters:X2}\\\"42\\\"{NumberOfCharacters:X2}\\\"12:25\\\"{NumberOfCharacters:X2}\\\"5\\\""), 
+                 new StringBuilder("{NumberOfCharacters:X2}\\\"42\\\"{NumberOfCharacters:X2}\\\"12:25\\\"{NumberOfCharacters:X2}\\\"5\\\""),
                  "02420512:25015"
              },
             
@@ -32,7 +32,7 @@ namespace Shared.Test.StringInseartService.DependentInseartTest
                  "%000010320113%10$10$00$60$t2{Time:t}%000330940113%10$10$00$60$t3{Event}%000951920114%10$10$00$60$t1{StationsCut}%000011920183%10$10$00$60$t2 ",
                  "{CRCXorInverse:X2}\u0003",
                  "Windows-1251",
-                 new StringBuilder("\u000205{Nchar:X2}%000010320113%10$10$00$60$t215:25%000330940113%10$10$00$60$tПриб%000951920114%10$10$00$60$t1Москва-Питер%000011920183%10$10$00$60$t2 {CRCXorInverse:X2}\u0003"), 
+                 new StringBuilder("\u000205{Nchar:X2}%000010320113%10$10$00$60$t215:25%000330940113%10$10$00$60$tПриб%000951920114%10$10$00$60$t1Москва-Питер%000011920183%10$10$00$60$t2 {CRCXorInverse:X2}\u0003"),
                  "\u00020585%000010320113%10$10$00$60$t215:25%000330940113%10$10$00$60$tПриб%000951920114%10$10$00$60$t1Москва-Питер%000011920183%10$10$00$60$t2 31\u0003"
              },
             
@@ -43,7 +43,7 @@ namespace Shared.Test.StringInseartService.DependentInseartTest
                  "0x03{Hour:D2}0x3A{Minute:D2}",
                  "0x{CRCMod256:X2}",
                  "cp866",
-                 new StringBuilder("0x080x{NbyteFull:X2}0x03100x3A450x{CRCMod256:X2}"), 
+                 new StringBuilder("0x080x{NbyteFull:X2}0x03100x3A450x{CRCMod256:X2}"),
                  "0x080x090x03100x3A450x18"
              },
             
@@ -54,7 +54,7 @@ namespace Shared.Test.StringInseartService.DependentInseartTest
                  "041FFFFF1010FEFE051F",
                  "",
                  "HEX",
-                 new StringBuilder("041FFFFF1010FEFE051F"), 
+                 new StringBuilder("041FFFFF1010FEFE051F"),
                  "041FFFFF1010FEFE051F"
              },
             
@@ -63,9 +63,9 @@ namespace Shared.Test.StringInseartService.DependentInseartTest
             {
                 "0xFF0xFF0x020x1B0x57",
                 "{Addition} {StationsCut}0x09{ExpectedTime:t}0x09{Note}0x09",
-                "0x030x{CRCXor[0x02-0x03]:X2}0x1F",
+                "0x030x{CRCXor:X2_Border}0x1F",
                 "Windows-1251",
-                new StringBuilder("0xFF0xFF0x020x1B0x57Дополн. Хабаровск0x0918:000x09Со всеми остановками0x090x030x{CRCXor[0x02-0x03]:X2}0x1F"), 
+                new StringBuilder("0xFF0xFF0x020x1B0x57Дополн. Хабаровск0x0918:000x09Со всеми остановками0x090x030x{CRCXor:X2_Border}0x1F"),
                 "0xFF0xFF0x020x1B0x57Дополн. Хабаровск0x0918:000x09Со всеми остановками0x090x030xA80x1F"
             },
             
@@ -76,7 +76,7 @@ namespace Shared.Test.StringInseartService.DependentInseartTest
                 "%30{SyncTInSec:X5}%010C60EF03B0470000001E%110406NNNNN%01000023{(rowNumber*24-23):X3}{(rowNumber*24-13):X3}0020001E%10{NumberOfCharacters:X2}01\\\"{NumberOfTrain}\\\"%01024030{(rowNumber*24-23):X3}{(rowNumber*24-13):X3}0000001E%10{NumberOfCharacters:X2}01\\\" \\\"%01062086{(rowNumber*24-23):X3}{(rowNumber*24-13):X3}0000001E%10{NumberOfCharacters:X2}01\\\"{TArrival:t}\\\"%010870AB{(rowNumber*24-23):X3}{(rowNumber*24-13):X3}0000001E%10{NumberOfCharacters:X2}01\\\"{TDepart:t}\\\"",
                 "{CRCXorInverse:X2}\u0003",
                 "Windows-1251",
-                new StringBuilder("\u000208{Nchar:X2}%3000125%010C60EF03B0470000001E%110406NNNNN%010000230010080020001E%10{NumberOfCharacters:X2}01\\\"856\\\"%010240300010080000001E%10{NumberOfCharacters:X2}01\\\" \\\"%01062086000080000001E%10{NumberOfCharacters:X2}01\\\"15:21\\\"%010870AB0010080000001E%10{NumberOfCharacters:X2}01\\\"20:50\\\"{CRCXorInverse:X2}\u0003"), 
+                new StringBuilder("\u000208{Nchar:X2}%3000125%010C60EF03B0470000001E%110406NNNNN%010000230010080020001E%10{NumberOfCharacters:X2}01\\\"856\\\"%010240300010080000001E%10{NumberOfCharacters:X2}01\\\" \\\"%01062086000080000001E%10{NumberOfCharacters:X2}01\\\"15:21\\\"%010870AB0010080000001E%10{NumberOfCharacters:X2}01\\\"20:50\\\"{CRCXorInverse:X2}\u0003"),
                 "\u000208B0%3000125%010C60EF03B0470000001E%110406NNNNN%010000230010080020001E%100301856%010240300010080000001E%100101 %01062086000080000001E%10050115:21%010870AB0010080000001E%10050120:5080\u0003"
             },
         };
@@ -103,7 +103,7 @@ namespace Shared.Test.StringInseartService.DependentInseartTest
         {
             //Arrange
             var header = "\u0002{AddressDevice:X2}{Nchar:X2}";
-            var body= "%30{SyncTInSec:X5}%010C60EF03B0470000001E%110406NNNNN%01000023{(rowNumber*24-23){NumberOfCharacters01\\\"78\\\"";
+            var body= "%30{SyncTInSec:X5}%010C60EF03B0470000001E%110406NNNNN%01000023{MATH(rowNumber*24-23){NumberOfCharacters01\\\"78\\\"";
             var footer= "{CRCXorInverse:X2\u0003";
             var strInit = header+body+footer;
             
